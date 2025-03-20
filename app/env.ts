a79c231c44a1cedc58664e6f18a90929d6ac4973
M Haidar Hanif <git@mhaidarhanif.com>
2025-03-21 05:55:04 +0700

    feat: fetch products data, type with openapi-ts, setup env

---
+++ app/env.ts
@@ -0,0 +1,1 @@
+export const backendApiUrl = process.env.BACKEND_API_URL;
--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -1,9 +1,7 @@
-import type { Route } from "./+types/home";
-
-import { Header } from "~/components/layout/header";
-import { Footer } from "~/components/layout/footer";
-
-import MainImage from "~/assets/home.jpeg";
+import { Link } from "react-router";
+import type { Route } from "./+types/products";
+import { backendApiUrl } from "~/env";
+import type { ProductsJSON } from "~/modules/product/type";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,10 +10,45 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Products() {
+export async function loader() {
+  const response = await fetch(`${backendApiUrl}/products`);
+  const products: ProductsJSON = await response.json();
+  return { products };
+}
+
+export default function Products({ loaderData }: Route.ComponentProps) {
+  const { products } = loaderData;
+
   return (
     <div>
       <h1>Products</h1>
+      <ul>
+        {products.map((product) => {
+          // TODO: Refactor to separate function
+          const productImageObject = product.images[0];
+
+          const imageUrl = productImageObject?.url
+            ? productImageObject.url
+            : "https://placehold.co/300x200";
+
+          const altText = productImageObject?.altText
+            ? productImageObject.altText
+            : product.name;
+
+          return (
+            <li key={product.id}>
+              <Link to={`/products/${product.slug}`}>
+                <div>
+                  {/* TODO: <ProductImage product={product} /> */}
+                  <img src={imageUrl} alt={altText} />
+                  <h2>{product.name}</h2>
+                  <p>{product.price}</p>
+                </div>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
