dbce30c609b8b9e76894154536035e24df1ff27c
M Haidar Hanif <git@mhaidarhanif.com>
2025-04-24 01:28:14 +0700

    refactor: remove unused auth layout

--- app/layouts/auth.tsx
+++
@@ -1,6 +0,0 @@
-import { Outlet } from "react-router";
-import type { Route } from "./+types/auth";
-
-export default function Layout({}: Route.ComponentProps) {
-  return <Outlet />;
-}
--- app/routes.ts
+++ app/routes.ts
@@ -6,24 +6,22 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-  layout("layouts/auth.tsx", [
-    layout("layouts/app.tsx", [
-      index("routes/home.tsx"),
-      route("products", "routes/products.tsx"),
-      route("products/:slug", "routes/products-slug.tsx"),
-      route("about", "routes/about.tsx"),
-      route("faq", "routes/faq.tsx"),
-      route("contact", "routes/contact.tsx"),
-      route("shops", "routes/shops.tsx"),
-      route("cart", "routes/cart.tsx"),
+  layout("layouts/app.tsx", [
+    index("routes/home.tsx"),
+    route("products", "routes/products.tsx"),
+    route("products/:slug", "routes/products-slug.tsx"),
+    route("about", "routes/about.tsx"),
+    route("faq", "routes/faq.tsx"),
+    route("contact", "routes/contact.tsx"),
+    route("shops", "routes/shops.tsx"),
+    route("cart", "routes/cart.tsx"),
 
-      // ✅ TODO
-      route("dashboard", "routes/dashboard.tsx"),
-      // route("cart", "routes/cart.tsx"),
-    ]),
-
-    route("register", "routes/register.tsx"),
-    route("login", "routes/login.tsx"),
-    route("logout", "routes/logout.tsx"),
+    // ✅ TODO
+    route("dashboard", "routes/dashboard.tsx"),
+    // route("cart", "routes/cart.tsx"),
   ]),
+
+  route("register", "routes/register.tsx"),
+  route("login", "routes/login.tsx"),
+  route("logout", "routes/logout.tsx"),
 ] satisfies RouteConfig;
