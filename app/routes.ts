7eec25799de826e6e65ce1c0a01b330b301e4905
M Haidar Hanif <git@mhaidarhanif.com>
2025-04-23 23:22:31 +0700

    feat: enable dashboard

--- app/routes.ts
+++ app/routes.ts
@@ -17,10 +17,11 @@ export default [
     route("cart", "routes/cart.tsx"),
 
     // ✅ TODO
-    // route("dashboard", "routes/dashboard.tsx"),
-    // route("logout", "routes/logout.tsx"),
+    route("dashboard", "routes/dashboard.tsx"),
     // route("cart", "routes/cart.tsx"),
   ]),
+
   route("register", "routes/register.tsx"),
   route("login", "routes/login.tsx"),
+  // route("logout", "routes/logout.tsx"),
 ] satisfies RouteConfig;
