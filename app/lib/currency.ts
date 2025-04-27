009424c5570b4f158a1f1a7c1ab29da07454367b
M Haidar Hanif <git@mhaidarhanif.com>
2025-04-27 12:25:12 +0700

    feat: enhance formatPrice function to accept dynamic currency parameter

--- app/lib/currency.ts
+++ app/lib/currency.ts
@@ -1,7 +1,10 @@
-export function formatPrice(price: number | string | unknown): string {
+export function formatPrice(
+  price: number | string | unknown,
+  currency = "EUR"
+): string {
   return new Intl.NumberFormat("fr-FR", {
     style: "currency",
-    currency: "EUR",
+    currency: currency,
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(Number(price));
